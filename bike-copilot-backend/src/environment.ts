a7e41cad308839c5dbdfe759fdf3598cebad55be
Riccardo Omiccioli <riccardo@omiccioli.it>
2023-05-08 16:46:46 +0200

    chore: Added backend and db

---
+++ bike-copilot-backend/src/environment.ts
@@ -0,0 +1,23 @@
+import {Time} from "./utils/time";
+
+export class Environment {
+
+    /** The host for the backend application. */
+    public static readonly APPLICATION_BACKEND_HOST: string = "127.0.0.1";
+    /** The port for the backend application. */
+    public static readonly APPLICATION_BACKEND_PORT: number = 3001;
+
+    /** The host for the frontend application. */
+    public static readonly APPLICATION_FRONTEND_HOST: string = "127.0.0.1";
+    /** The port for the frontend application. */
+    public static readonly APPLICATION_FRONTEND_PORT: number = 3000;
+
+    /** The host for the backend database. */
+    public static readonly DATABASE_BACKEND_HOST: string = "127.0.0.1";
+    /** The port for the backend database. */
+    public static readonly DATABASE_BACKEND_PORT: number = 27017;
+
+    /** The duration in milliseconds of a new session before expiration. */
+    public static readonly SESSION_DURATION: number = 1 * Time.Days;
+
+}
