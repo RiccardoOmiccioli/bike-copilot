98a8cd2364d1c3846a745de275915ce131794ad6
Riccardo Omiccioli <omiccioli.riccardo@gmail.com>
2024-06-05 14:38:30 +0200

    update sprite location

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from "./router";
-// import './assets/style/material_theme.css';
 import './assets/style/theme.css'
 
 import mitt from "mitt";
